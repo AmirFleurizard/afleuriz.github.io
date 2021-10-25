f47ed40f83975f76a3101d5980c5ca4d7ab1ee6d
AmirFleurizard <afleuriz@uncc.edu>
2021-10-24 23:52:28 -0400

    trying to edit variable

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -24,9 +24,8 @@ function imageLink()
 	}else{
 		
 	}
+}*/
 	
-}
-*/
 	
 $(document).ready(function()
 {
@@ -35,31 +34,30 @@ $(document).ready(function()
 	$('.next').on('click', function()
 	{
 		
-		var currentImage = $('.active');
-		var nextImage = currentImage.next();
+		var currentImg = $('.active');
+		var nextImg = currentImg.next();
 		
-		if(nextImage.length){
-			
-			currentImage.removeClass('active').css('z-index', -10);
-			nextImage.addClass('active').css('z-index', 10);
-			
-		}
-	});
-	
-	$('.previous').on('click', function()
-	{
-		
-		var currentImage = $('.active');
-		var previousImage = currentImage.previous();
-		
-		if(previousImage.length)
+		if(nextImg.length)
 		{
 			
-			currentImage.removeClass('active').css('z-index', -10);
-			previousImage.addClass('active').css('z-index', 10);
+			currentImg.removeClass('active').css('z-index', -10);
+			nextImg.addClass('active').css('z-index', 10);
 			
 		}
-		
 	});
 	
+	$('.prev').on('click', function()
+	{
+		
+		var currentImg = $('.active');
+		var prevImg = currentImg.prev();
+		
+		if(prevImg.length)
+		{
+			
+			currentImg.removeClass('active').css('z-index', -10);
+			prevImg.addClass('active').css('z-index', 10);
+			
+		}
+	});
 });
