19dd71aa34ab5d4de37e00f12d3105b0e9703b8b
AmirFleurizard <afleuriz@uncc.edu>
2021-10-24 23:04:25 -0400

    slideshow

---
+++ itis3135/scripts/slideshow.js
@@ -0,0 +1,53 @@
+function alerts()
+{
+
+	alert("Hey my script is running");
+
+}
+
+function imageLink()
+{
+	
+	var currentImageClass = document.getElementsByClassName("active");
+	
+	if(currentImageClass.getAttribute('alt') == "Image 1"){
+		
+	}else if('.active'.image.getAttribute('alt'); == "Image 2"){
+		
+	}else if('.active'.alt == "Image 3"){
+		
+	}else if('.active'.alt == "Image 4"){
+		
+	}else if('.active'.alt == "Image 5"){
+		
+	}else{
+		
+	}
+	
+}
+	
+$(document).ready(function(){
+	
+	
+	$('.next').on('click', function(){
+		
+		var currentImg = $('.active');
+		var nextImg = currentImg.next();
+		
+		if(nextImg.length){
+			currentImg.removeClass('active').css('z-index', -10);
+			nextImg.addClass('active').css('z-index', 10);
+		}
+	});
+	
+	$('.prev').on('click', function(){
+		
+		var currentImg = $('.active');
+		var prevImg = currentImg.prev();
+		
+		if(prevImg.length){
+			currentImg.removeClass('active').css('z-index', -10);
+			prevImg.addClass('active').css('z-index', 10);
+		}
+	});
+});
