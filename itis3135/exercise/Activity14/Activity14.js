fc819d1f880c20a19aa1fdcde79b9c1cf24619d5
AmirFleurizard <afleuriz@uncc.edu>
2021-11-21 21:53:57 -0500

    Update Activity14.js

--- itis3135/exercise/Activity14/Activity14.js
+++ itis3135/exercise/Activity14/Activity14.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
 	
+	$("#nav_list li".click(function(){
+		var title = $(this).children("a").attr("title");
+		var filename = title + ".json";
+		consumeJSON(filename);
+		});
+	
 }); // end ready
+
+function consumeJSON(jsonURL)
+{
+	
+	$.ajax({
+		url: jsonURL,
+		dataType: "text",
+		success: function (data) 
+		{
+			
+			var json = $.parseJSON(data);
+			$("main > h2").html(json.speakers[0].month + "<br/>" + json.speakers[0].speaker);
+			$("main > h1").html(json.speakers[0].title);
+			$("main > img").attr("src", json.speakers[0].image);
+			$("main > p").html(json.speakers[0].text);
+			
+		}
+	});
+}
+
