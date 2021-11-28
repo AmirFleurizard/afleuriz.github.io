ba610f075e1858e597282913f32e8e92b679d2a4
AmirFleurizard <afleuriz@uncc.edu>
2021-11-28 11:27:09 -0500

    activity15

---
+++ itis3135/exercise/Activity15/Fleurizard-Activity15.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$.getJson("facultyList.json", function(data){
+		$.each(data, function(){
+			$.each(this, function(key, value){
+				$("faculty").append(
+				"<img src=" + "'" + value.image + "'><br>" +
+				"<h2>" + value.full_name + "</h2>" +
+				"<h3>" + value.department + "</h3>" +
+				"<p>" + value.bio + "</p><br>"
+				);
+			});
+		});
+	});
+
+});
