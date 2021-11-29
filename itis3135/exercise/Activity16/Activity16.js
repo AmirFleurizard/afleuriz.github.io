f8052de5842be4a7548fc3ceb1934e906912d5bd
AmirFleurizard <afleuriz@uncc.edu>
2021-11-28 22:15:10 -0500

    16!

---
+++ itis3135/exercise/Activity16/Activity16.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){ 
+    var url = 
+        "http://api.flickr.com/services/feeds/photos_public.gne?" + 
+        "id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding"; 
+ 
+    $.getJSON(url, function(data) { 
+        var html = ""; 
+        $.each(data.items, function(i, item){ 
+            html += "<a href= '" + item.link + "'";
+			html += "data-lightbox='images'";
+			html += "data-title='" + item.title + "'>";
+			html += "<img src='" + item.media.m + "'/></a>"
+             
+			
+			
+			
+            // Remove the first paragraph of the description 
+            /*html = html.replace( 
+                   "<p><a href=\"http://www.flickr.com/people/" + 
+                   "82407828@N07/\">zakruvalcaba<\/a>" + 
+                   " posted a photo:<\/p>", ""); */
+        }); 
+        $("#new_building").html(html); 
+    }); 
+});
