bb23a01d55cd264b57c25f5e7c324abf6f980f4a
AmirFleurizard <afleuriz@uncc.edu>
2021-12-05 13:18:43 -0500

    a3

--- itis3135/project/scripts/portfolio.js
+++ itis3135/project/scripts/portfolio.js
@@ -1,4 +1,4 @@
-function displayInfo()
+/*function displayInfo()
 {
 	
 	var firstName = document.getElementById("fName").value;
@@ -11,4 +11,46 @@ function displayInfo()
 	document.getElementById("resultEmail").innerHTML = "Email: " + email;
 	document.getElementById("resultMessage").innerHTML = "Message: " + message;
 	
+}*/
+
+$(document).ready(function() {
+	$("#submit").click(function() 
+	{
+		var name = $("#name").val();
+		var email = $("#email").val();
+		var message = $("#message").val();
+		var contact = $("#contact").val();
+		$("#returnmessage").empty();
+
+				$.post("contact_me.php", {
+					name1: name,
+					email1: email,
+					message1: message,
+					contact1: contact
+					}, function(data) 
+					{
+						$("#returnmessage").append(data);
+						if (data == "Your message has been received! I will contact you soon.") 
+						{
+							$("#form")[0].reset();
+						}
+					});
+			
+	});
+});
+
+function alerts(){
+
+	alert("Hey my script is running");
+
 }
+	
+<!-- jQuery call to the accordion() method. -->
+$(document).ready(function() {
+	$('#accordion').accordion({
+		
+		active: false,
+		collapsible: true
+		
+	});
+});
